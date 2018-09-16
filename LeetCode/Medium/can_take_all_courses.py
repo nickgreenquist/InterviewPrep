c7051d58b8d2b95a09e0f9024c5a579fcb77866d
nickgreenquist <nickgreenquist@gmail.com>
2018-09-16 13:40:05 -0400

    Medium level algorithm using top sort to see if all courses can be taken

---
+++ LeetCode/Medium/can_take_all_courses.py
@@ -0,0 +1,36 @@
+
+def canFinish(numCourses, prerequisites):
+    
+    adj_list = {}
+    for p in prerequisites:
+        source = p[1]
+        dest = p[0]
+        if source in adj_list:
+            adj_list[source].append(dest)
+        else:
+            adj_list[source] = [dest]
+            
+    inblocks = {}
+    for k,v in adj_list.items():
+        for dest in v:
+            if dest in inblocks:
+                inblocks[dest] += 1
+            else:
+                inblocks[dest] = 1
+    
+    q = []
+    for i in range(numCourses):
+        if i not in inblocks:
+            q.append(i)
+    
+    count = 0
+    while len(q) > 0:
+        course = q.pop(0)
+        count += 1
+        if course in adj_list:
+            for dest in adj_list[course]:
+                inblocks[dest] -= 1
+                if inblocks[dest] == 0:
+                    q.append(dest)
+    
+    return count == numCourses
