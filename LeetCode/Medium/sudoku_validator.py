6c7254d3da236a8b8a7989fe3218b8b62783c437
nickgreenquist <nickgreenquist@gmail.com>
2018-10-04 21:56:56 -0400

    Medium: one pass sudoko board validator

---
+++ LeetCode/Medium/sudoku_validator.py
@@ -0,0 +1,66 @@
+
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+    
+def checkBoard(board, row, row_seen, col_seen, grid_seen):
+    for col in range(9):
+        num = board[row][col]
+        if not is_number(num):
+            continue
+        
+        # check for valid row and col and grid
+        if num in row_seen[row] or num in col_seen[col] or num in grid_seen[row//3][col//3]:
+            return False
+        row_seen[row].add(num)
+        col_seen[col].add(num)
+        grid_seen[row//3][col//3].add(num)
+        
+    return True
+    
+    
+def isValidSudoku(board):
+    row_seen = [set() for i in range(9)]
+    col_seen = [set() for i in range(9)]
+    grid_seen = [[set() for j in range(3)] for i in range(3)]
+    for i in range(9):
+        if not checkBoard(board, i, row_seen, col_seen, grid_seen):
+            return False
+    return True
+
+'''
+Example 1:
+Output: true
+'''
+print(isValidSudoku([
+  ["5","3",".",".","7",".",".",".","."],
+  ["6",".",".","1","9","5",".",".","."],
+  [".","9","8",".",".",".",".","6","."],
+  ["8",".",".",".","6",".",".",".","3"],
+  ["4",".",".","8",".","3",".",".","1"],
+  ["7",".",".",".","2",".",".",".","6"],
+  [".","6",".",".",".",".","2","8","."],
+  [".",".",".","4","1","9",".",".","5"],
+  [".",".",".",".","8",".",".","7","9"]
+]))
+
+'''
+Example 2:
+Output: false
+Explanation: Same as Example 1, except with the 5 in the top left corner being 
+    modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+'''
+print(isValidSudoku([
+  ["8","3",".",".","7",".",".",".","."],
+  ["6",".",".","1","9","5",".",".","."],
+  [".","9","8",".",".",".",".","6","."],
+  ["8",".",".",".","6",".",".",".","3"],
+  ["4",".",".","8",".","3",".",".","1"],
+  ["7",".",".",".","2",".",".",".","6"],
+  [".","6",".",".",".",".","2","8","."],
+  [".",".",".","4","1","9",".",".","5"],
+  [".",".",".",".","8",".",".","7","9"]
+]))
