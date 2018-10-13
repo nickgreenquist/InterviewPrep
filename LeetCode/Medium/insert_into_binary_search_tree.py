0cbe4715d46842b648a2028bfc014fe145972539
nickgreenquist <nickgreenquist@gmail.com>
2018-10-13 12:26:27 -0400

    Easy (but not with O(1) space): Find the missing numbers in an array of size N where all numbers 1-N should be

--- LeetCode/Medium/insert_into_binary_search_tree.py
+++ LeetCode/Medium/insert_into_binary_search_tree.py
@@ -1,14 +1,20 @@
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
 def insertIntoBST(root, val):
     if not root:
         root = TreeNode(val)
     elif val < root.val:
         if root.left:
-            self.insertIntoBST(root.left, val)
+            insertIntoBST(root.left, val)
         else:
             root.left = TreeNode(val)
     else:
         if root.right:
-            self.insertIntoBST(root.right, val)
+            insertIntoBST(root.right, val)
         else:
             root.right = TreeNode(val)
             
