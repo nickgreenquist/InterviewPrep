97dccf520324d5d7ed902b3d1e04aba5bd2d4178
nickgreenquist <nickgreenquist@gmail.com>
2018-10-21 22:08:30 -0400

    force commit to test repo

--- LeetCode/Medium/reordered_power_of_2.py
+++ LeetCode/Medium/reordered_power_of_2.py
@@ -1,6 +1,5 @@
 '''
 Starting with a positive integer N, we reorder the digits in any order (including the original order) such that the leading digit is not zero.
-
 Return true if and only if we can do this in a way such that the resulting number is a power of 2.
 '''
 
