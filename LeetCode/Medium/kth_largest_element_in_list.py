620ac9d60e151b5601cc7046c06df26cd979d045
nickgreenquist <nickgreenquist@gmail.com>
2018-10-27 14:04:45 -0400

    Medium: updated to use QuickSelect O(n) algo vs original naive sort O(nlogn) algo

--- LeetCode/Medium/kth_largest_element_in_list.py
+++ LeetCode/Medium/kth_largest_element_in_list.py
@@ -1,13 +1,35 @@
-import heapq
+from random import randint
 
 def findKthLargest(nums, k):
-    heap = []
-    for num in nums:
-        heapq.heappush(heap, -num)
-    while k > 0:
-        val = -heapq.heappop(heap)
-        k -= 1
-    return val
+    def quick_select(nums, k):
+        n = len(nums)
+        if len(nums) == 1:
+            return nums[0]
+        
+        p = randint(0, n-1)
+        x = nums[p]
+        
+        L = []
+        E = []
+        G = []
+        for num in nums:
+            if num < x:
+                L.append(num)
+            elif num == x:
+                E.append(num)
+            else:
+                G.append(num)
+        
+        if k <= len(G):
+            return quick_select(G, k)
+        elif k <= len(G) + len(E):
+            return x
+        else:
+            return quick_select(L, k - len(G) - len(E))
+    
+    return quick_select(nums, k)
+            
+        
 
 '''
 Input: [3,2,3,1,2,4,5,5,6] and k = 4
