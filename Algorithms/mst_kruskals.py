0375c3cf16d7c7a9bb59813227b2d93cd5defe5e
nickgreenquist <nickgreenquist@gmail.com>
2022-07-01 10:20:16 -0400

    update kruskals mst to use standalone UnionFind data structure to make it more readable

--- Algorithms/mst_kruskals.py
+++ Algorithms/mst_kruskals.py
@@ -1,84 +1,102 @@
-from collections import defaultdict
+class UnionFind:
+    def __init__(self, num_vertices):
+        self.num_vertices = num_vertices
+        self.size = [1 for i in range(num_vertices)]
+        self.parent = [i for i in range(num_vertices)]
+        self.numComponents = num_vertices
+
+    def find(self, v):
+        root = v
+        while root != self.parent[root]:
+            root = self.parent[root]
+        
+        # compress paths
+        while v != root:
+            next = self.parent[v]
+            self.parent[v] = root
+            v = next
+        return root
+
+    def union(self, x, y):
+        rootX = self.find(x)
+        rootY = self.find(y)
+        if rootX == rootY:
+            return
+        
+        # merge smaller path into larger path
+        if self.size[rootX] < self.size[rootY]:
+            self.parent[rootX] = rootY
+            self.size[rootY] += self.size[rootX]
+        else:
+            self.parent[rootY] = rootX
+            self.size[rootX] += self.size[rootY]
+        
+        self.numComponents -= 1
+    
+    def isConnected(self, x, y):
+        return self.find(x) == self.find(y)
+
 
 class Graph(object):
     def __init__(self, num_vertices):
         self.V = num_vertices
-        self.G = [] #adjacency list
+        self.edges = [] # list of single tuple edges
 
     def addEdge(self, u, v, w):
-        # store triple containing the edge
-        self.G.append([u,v,w])
+        # store triple containing the edge + weight
+        self.edges.append([u,v,w])
 
-    # find the set of an element i
-    def find_parent(self, parent, i):
-        # check if you are your current parent
-        if parent[i] == i:
-            return i
-        else:
-            return self.find_parent(parent, parent[i])
+def kruskal_mst(G):
 
-    # union of x's set and y's set using the rank
-    def union(self, parent, rank, x, y):
-        parent_x = self.find_parent(parent, x)
-        parent_y = self.find_parent(parent, y)
+    # Step 1: sort all edges in non-decreasing order of weights
+    edges = sorted(G.edges, key=lambda x: x[2])
 
-        # append the smaller rank tree under the root of the higher rank tree
-        if rank[parent_x] < rank[parent_y]:
-            parent[parent_x] = parent_y
-        elif rank[parent_x] > rank[parent_y]:
-            parent[parent_y] = parent_x
-        else:
-            # either can be parent, just increaes it's rank
-            parent[parent_y] = parent_x
-            rank[parent_x] += 1
+    uf = UnionFind(G.V)
+    mst_edges = []
 
-    def kruskal(self):
-        mst = []
+    # Step 2: Loop over all sorted edges and union x and y if not connected.
+    for edge in edges:
+        x = edge[0]
+        y = edge[1]
+        w = edge[2]
 
-        # Step 1: sort all edges in non-decreasing order of weights
-        self.G = sorted(self.G, key=lambda x: x[2])
+        rootX = uf.find(x)
+        rootY = uf.find(y)
 
-        parent = []
-        rank = []
+        if rootX == rootY:
+            continue
+        
+        uf.union(x, y)
+        mst_edges.append([x, y, w])
 
-        for v in range(self.V):
-            parent.append(v)
-            rank.append(0)
-
-        i = 0
-        e = 0
-        while e < self.V - 1:
-            # Step 2: pick the smallest edge
-            u,v,w = self.G[i]
-            i += 1
-
-            parent_u = self.find_parent(parent, u)
-            parent_v = self.find_parent(parent, v)
-
-            # check if this edge causes a cycle
-            if parent_u != parent_v:
-                e += 1 # select this edge
-                mst.append([u,v,w])
-                self.union(parent, rank, parent_u, parent_v)
-
-        return mst
+    return mst_edges
 
 # Driver code to test MST
-g = Graph(4) 
-g.addEdge(0, 1, 10) 
-g.addEdge(0, 2, 6) 
-g.addEdge(0, 3, 5) 
-g.addEdge(1, 3, 15) 
+g = Graph(6) 
+
+g.addEdge(0, 1, 7) 
+g.addEdge(0, 2, 8) 
+
+g.addEdge(1, 2, 3) 
+g.addEdge(1, 3, 6) 
+g.addEdge(1, 5, 15)
+
 g.addEdge(2, 3, 4) 
-  
-mst = g.kruskal() 
-print(mst)
+g.addEdge(2, 5, 3) 
+
+g.addEdge(3, 4, 5) 
+g.addEdge(3, 5, 2) 
+
+g.addEdge(4, 5, 2)
+
+mst_edges = kruskal_mst(g)
+print(sorted(mst_edges, key=lambda x: x[0]))
 
 '''
 Time Complexity: O(ElogE) or O(ElogV).
 Sorting of edges takes O(ELogE) time.
 After sorting, we iterate through all edges and apply find-union algorithm.
-The find and union operations can take atmost O(LogV) time.
+The find and union operations take amortized O(1) time. 
 So overall complexity is O(ELogE + ELogV) time.
 
 The value of E can be atmost O(V2), so O(LogV) and O(LogE) are the same.
