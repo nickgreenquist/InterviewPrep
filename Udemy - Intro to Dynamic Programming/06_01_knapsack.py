a222aafca9521d40b66e2958f7e3cb897e353d7b
nickgreenquist <nickgreenquist@gmail.com>
2022-09-17 10:49:48 -0400

    Classic 01 Knapsack code

--- Udemy - Intro to Dynamic Programming/06_01_knapsack.py
+++ Udemy - Intro to Dynamic Programming/06_01_knapsack.py
@@ -1,5 +1,32 @@
 def knapSack(W, wt, val, n):
-    pass
+    dp = []
+    for i in range(n+1):
+        dp.append([])
+        for j in range(W+1):
+            dp[i].append(0)
+    
+    # bottom up knapsack
+    for i in range(n+1):
+        for w in range(W+1):
+            # base case
+            if i == 0 or w == 0:
+                dp[i][w] = 0
+                continue
+            
+            # if wt[i-1] doesn't fit into curent weight limit
+            if wt[i-1] > w:
+                dp[i][w] = dp[i-1][w]
+                continue
+            
+            # if the value of this item plus the best total value
+            # from the last item minus this item's weight is better
+            # than best total value from last item at this weight limit
+            if val[i-1] + dp[i-1][w - wt[i-1]] > dp[i-1][w]:
+                dp[i][w] = val[i-1] + dp[i-1][w - wt[i-1]]
+            else:
+                dp[i][w] = dp[i-1][w]
+    
+    return dp[n][W]
 
 
 # Driver program to test above function 
